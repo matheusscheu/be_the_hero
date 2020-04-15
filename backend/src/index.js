fea5322849c747dfbe3a7cf5b197c48b1f0c42f9
Matheus Scheunemann <matheuss.sls@gmail.com>
2020-04-15 09:33:28 -0300

    backend feito

---
+++ backend/src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+const routers = require('./routes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routers);
+
+app.listen(3333);
